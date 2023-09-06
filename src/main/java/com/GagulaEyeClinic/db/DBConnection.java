9792d45cf3e3bc6c8aa22782adad767ce775f363
Sam1ndaFernando <uthpala2001@gmail.com>
2023-09-06 07:48:08 +0530

    db connection logic created and dependency added.

--- src/main/java/com/GagulaEyeClinic/db/DBConnection.java
+++ src/main/java/com/GagulaEyeClinic/db/DBConnection.java
@@ -1,4 +1,22 @@
 package com.GagulaEyeClinic.db;
 
+        import java.sql.Connection;
+        import java.sql.DriverManager;
+        import java.sql.SQLException;
+
 public class DBConnection {
+    private static DBConnection dbConnection;
+    private Connection connection;
+
+    private DBConnection() throws ClassNotFoundException, SQLException, SQLException, SQLException {
+        //Class.forName("com.mysql.cj.jdbc.Driver");
+
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/eyeClinic?allowPublicKeyRetrieval=true&useSSL=false", "root", "1234");
+    }
+    public static DBConnection getInstance() throws SQLException, ClassNotFoundException {
+        return (null == dbConnection) ? dbConnection = new DBConnection() : dbConnection;
+    }
+    public Connection getConnection() {
+        return connection;
+    }
 }
